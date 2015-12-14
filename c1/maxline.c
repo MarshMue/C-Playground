6e42e594b5dd1f451915f9aa242654424d0fe128
marshallmueller <mashymue@gmail.com>
2015-12-13 23:09:44 -0600

    finished maxline.c

--- c1/maxline.c
+++ c1/maxline.c
@@ -2,8 +2,8 @@
 
 #define MAXLINE 1000
 
-int getline(char line[], int maxline);
-void copy(char to[], char from[]);
+int m_getline(char line[], int maxline);
+void m_copy(char to[], char from[]);
 
 /* print longest input line */
 main() {
@@ -13,10 +13,10 @@ main() {
 	char longest[MAXLINE]; //longest line
 
 	max = 0;
-	while ((len = getline(line, MAXLINE)) > 0)
+	while ((len = m_getline(line, MAXLINE)) > 0)
 		if (len > max) {
 			max = len;
-			copy(longest, line);
+			m_copy(longest, line);
 		}
 	if (max > 0)
 		printf("%s", longest);
@@ -25,4 +25,32 @@ main() {
 
 /*
  * method getline reads a line into s 
- * param s
+ * param s is the line to be read it
+ * pram lim is the limit of the input line
+ * return - the size of the line
+ */
+int m_getline(char s[], int lim) {
+	int c, i;
+
+	for (i = 0; i < lim - 1 && (c=getchar()) != EOF && c !='\n'; ++i)
+		s[i] = c;
+	if (c == '\n') {
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+/*
+ * method copy copys a string
+ * param to - the string to be copied into
+ * param from - the string being copied
+ */
+void m_copy(char to[], char from[]) {
+	int i;
+
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+		i++;
+}
